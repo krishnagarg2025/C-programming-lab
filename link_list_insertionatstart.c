9aed75eda74a3d8c50d3607983a49747f07a5967
Krishna Garg <krishna.garg.cse.2025@miet.ac.in>
2026-08-25 11:46:07 +0530

    Add files via upload

---
+++ link_list_insertionatstart.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node{
+    int data;
+    struct node *next;
+};
+int main(){
+    struct node *head= NULL,*new= NULL,*temp=NULL;
+    int ch;
+    do{
+        new= malloc(sizeof(struct node*));
+        printf("Enter the element to be inserted in linklist\n");
+        scanf("%d",&new->data);
+        new->next = NULL;
+        if(head==NULL){
+            head= new;
+            temp= new;
+        }
+        else{
+            temp->next= new;
+            temp=temp->next;
+        }
+        printf("Do you want to add more data, enter 1, enter 0 to stop\n");
+        scanf("%d",&ch);
+    } while (ch==1);
+    struct node *p;
+    p=head;
+    printf("Given linklist is:\n");
+    while(p!=NULL){
+        printf("%d\n",p->data);
+        p=p->next;
+    }
+
+    new= malloc(sizeof(struct node*));
+    printf("Enter the element to be inserted at start of linklist\n");
+    scanf("%d",&new->data);
+    new->next=head;
+    head=new;
+
+    p=head;
+    printf("Linklist after insertion at start:\n");
+    while(p!=NULL){
+        printf("%d\n",p->data);
+        p=p->next;
+    }
+
+
+
+    return 0;
+}
